251ce8c4b6481babe65e1cc70928772f04e08a85
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-08 00:14:40 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily:{
+      'display':['Rubik','sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
