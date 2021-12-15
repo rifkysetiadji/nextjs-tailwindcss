5586faf55b3d49624fcbf74be20df767d376fe2c
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-15 18:37:34 +0700

    update dots

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Carousel from '../components/Carousel'
+import dynamic from 'next/dynamic'
 
+// import Carousel from '../components/Carousel'
+const Carousel = dynamic(()=> import('../components/Carousel'),
+{ loading: () => <div style={{textAlign:'center'}}><p>Loading</p></div> }
+)
 export default function Index() {
   return (
     <div>
