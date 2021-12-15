4f4d9fdda3ba5fc5e5b5a35272d7f10f1761ae40
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-15 17:33:07 +0700

    update dots

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Carousel from '../components/Carousel'
+// import Carousel from '../components/Carousel'
+import dynamic from 'next/dynamic'
+const Carousel = dynamic(()=>import('../components/Carousel'))
 export default function Index() {
   return (
     <div>
