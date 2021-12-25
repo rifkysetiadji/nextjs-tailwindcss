98ba0adf264bf0db40ac4cb9ebc5e5d913123a62
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-25 15:55:44 +0700

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index() {
   return (
     <div>
       <Layout>
-        {/* <Carousel/>  */}
+        <Carousel/> 
         <Services/>
         <Assesstment/>
         <Client/>
