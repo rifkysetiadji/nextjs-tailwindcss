fba4a8284335f54929ae1d67b0ff0047d08ca033
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-25 15:24:44 +0700

    add dynamic import client

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Layout from '../components/Layout'
 import Carousel from '../components/Carousel'
 import Services from '../components/Services'
 import Assesstment from '../components/Assesstment'
-import Client from '../components/Client'
+// import Client from '../components/Client'
+import dynamic from 'next/dynamic'
+const Client = dynamic(()=>import('../components/Client'))
+
 export default function Index() {
   return (
     <div>
