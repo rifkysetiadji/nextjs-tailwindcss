98c9433da69d86c2e1e2c59c1ed8284924559ada
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-26 15:53:49 +0700

    remove carousel

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Index() {
   return (
     <div>
       <Layout>
-        <Carousel/> 
+        {/* <Carousel/>  */}
         <Services/>
         <Assesstment/>
         <Client/>
